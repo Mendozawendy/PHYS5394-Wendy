67f7584695a7869b2afea978d7179afcf7afeb3a
soumya <soumya.d.mohanty@gmail.com>
2022-02-14 16:06:46 -0600

    SDM comments as FIXME statements.

--- Lab 2/gensinusig.m
+++ Lab 2/gensinusig.m
@@ -1,12 +1,17 @@
 function sigVec = gensinusig(dataX,snr,freq0,phi0) 
+%FIXME I prettyfied the comments by indenting the definition of the input
+%parameters
 % Generate a Sinusoidal signal 
 % S = GENSINUSIG(X,SNR,FREQ0,PHI0) 
 % Generate a Sinusoidal signal of S. 
 % data X is the vector of time stamps at which the value of signal is to be
-% computed. SNR is the matched filtering signal to noise of S. 
-% Freq0 is the frequency of the sinusoidal oscillation
-% Phi0 is the initial phase of the oscillation. 
-% s(t) = Asin(2*pi*freq0*t+phi0) 
+% computed. 
+%   SNR is the matched filtering signal to noise ratio of S. 
+%   Freq0 is the frequency of the sinusoidal oscillation
+%   Phi0 is the initial phase of the oscillation. 
+%FIXME The reader will not know that A is SNR: You can say the
+%"unnormalized signal is defined as ..." and remove A from the definition
+%s(t) = Asin(2*pi*freq0*t+phi0) 
 
 % Wendy Mendoza, Jan 2022 
 phaseVec = 2*pi*freq0*dataX+phi0;
--- Lab 2/testgensinusig.m
+++ Lab 2/testgensinusig.m
@@ -1,11 +1,17 @@
 %% Plot the Sinusoidal Signal function
 % Signal parameters
+%FIXME documentation should explain what the unit for frequency used here
+%is (i.e., Hz)
 freq0 = 11; %Freq0:11 been the best optional to see signal process. 
 phi0 = pi/2;
 A = 10;
 % Instantaneous frequency after 1 sec is 
+%FIXME The lab instruction required setting the sampling interval yourself:
+%The sqrt(5) factor makes no sense here. See the next FIXME comment.
 maxFreq = sqrt(5)*freq0;
 samplFreq = 5*maxFreq;
+%FIXME Specifying the sampling interval for this lab by hand...
+samplFreq = 30;%Hz
 samplIntrvl = 1/samplFreq;
 
 % Time samples
@@ -20,4 +26,5 @@ sigVec = gensinusig(timeVec,A,freq0,phi0);
 figure;
 plot(timeVec,sigVec,'Marker','.','MarkerSize',24);
 title('Sinusoidal Signal'); 
+%FIXME Label the X-axis ('Time (sec)')
  
