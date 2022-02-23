1dd3173a3067f52ee9a4ca7955ee5cde2998d3ad
soumya <soumya.d.mohanty@gmail.com>
2022-02-23 17:52:35 -0600

    SDM comments and changes.

--- L4Lab/testgensinusig.m
+++ L4Lab/testgensinusig.m
@@ -9,12 +9,14 @@ A = 10;
 maxFreq = freq0; %Maxmimum is equal to the constant frequency of the sinusoid
 
 %5 times the Nyquist sampling frequency
-samplFreq = 5*maxFreq;
+%SDM: The Nyquist frequency is 2*maxFreq
+samplFreq = 5*(2*maxFreq);
 samplFreq = 30; %Hz
 samplIntrvl = 1/samplFreq;
 
 %1/2 of the Nyquist sampling frequency
-samplFreq_2 = 1/2*maxFreq;
+%SDM: The Nyquist frequency is 2*maxFreq
+samplFreq_2 = 1/2*(2*maxFreq);
 samplIntrvl2 = 1/samplFreq_2;
 
 % Time samples
@@ -32,6 +34,13 @@ sigVec_2 = gensinusig(timeVec_2,A,freq0,phi0);
 %Plot the signal 
 figure;
 plot(timeVec,sigVec,'Marker','.','MarkerSize',24);
+title('Sinusoidal Signal'); 
+xlabel('Time (sec)'); 
+ylabel('Amplitude');
+
+%SDM Plot of the second signal
+figure;
+plot(timeVec_2,sigVec_2,'Marker','.','MarkerSize',24);
 title('Sinusoidal Signal'); 
 xlabel('Time (sec)'); 
 ylabel('Amplitude');
@@ -56,6 +65,8 @@ fftSig = fftSig(1:kNyq);
 fftSig_2 = fftSig_2(1:N_kNyq);
 
 %Plot periodogram
+%FIXME These are periodograms, not the time series plots. Correct the plot
+%labeling.
 figure;
 plot(posFreq,abs(fftSig),'Marker','.','MarkerSize',24);
 title('Sinusoidal Signal Sampling 5 times Nyquist sampling Frequency'); 
